70210c1c4422f22912b04e00e0a07fc36a0b995d
Sathwik-prog <bondalapatiradhika1989@gmail.com>
2026-02-08 16:51:20 +0530

    ez

---
+++ main2.py
@@ -0,0 +1,10 @@
+tuple1 = ('P', 'Y', 'T', 'H', 'O', 'N')
+
+# access the first element
+print(tuple1[0])
+
+# access the fourth element
+print(tuple1[3])
+
+# access the last element
+print(tuple1[-1])  
---
+++ main3.py
@@ -0,0 +1,5 @@
+tuple1 = ('P', 'Y', 'T', 'H', 'O', 'N')
+tuple2 = (1, 2, 3)
+
+# concatenation
+print(tuple1 + tuple2)
